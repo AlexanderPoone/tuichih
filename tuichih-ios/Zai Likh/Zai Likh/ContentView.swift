21f4419e5659193db87f3c96a71e1f613d08343e
Alex Poon <mundanesurrealism@gmail.com>
2019-09-10 00:26:21 +0800

    .

--- tuichih-ios/Zai Likh/Zai Likh/ContentView.swift
+++ tuichih-ios/Zai Likh/Zai Likh/ContentView.swift
@@ -7,7 +7,6 @@
 //
 
 import SwiftUI
-import SwiftDrawer
 
 private let dateFormatter: DateFormatter = {
     let dateFormatter = DateFormatter()
---
+++ tuichih-ios/Zai Likh/Zai Likh/TwoBarController.swift
@@ -0,0 +1,16 @@
+//
+//  TwoBarController.swift
+//  Zai Likh
+//
+//  Created by Victor Poon on 9/9/2019.
+//  Copyright © 2019 SoftFeta. All rights reserved.
+//
+
+import UIKit
+import SwiftUI
+
+struct TwoBarController: UIViewControllerRepresentable {
+    typealias UIViewControllerType = <#type#>
+    
+
+}
