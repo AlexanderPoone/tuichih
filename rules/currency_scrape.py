324c2544e26b7a2b70848a579ec572aaab837fae
Alexis Poon <alexander@link.cuhk.edu.hk>
2019-09-27 14:14:37 +0800

    .

--- rules/currency_scrape.py
+++ rules/currency_scrape.py
@@ -103,7 +103,7 @@ while True:
     # for k, v in doneCurrencyNames.items():
     #     if v['symbol'] == inputSymb:
     #         print(k)
-    inputSen = input('Insert a sentence: ')
+    inputSen = input('Insert a sentence (e.g. I have 5,000 groszy.): ')
     for k, v in doneCurrencyNames.items():
         if v['symbol'] is not None:
             if isinstance(v['symbol'],list):
@@ -113,7 +113,7 @@ while True:
             nounCurrency = s('^.* ','',k,flags=IGNORECASE)
             if k == 'Euro':                                                                                 # exception: only one-word currency: euro
                 nounCurrency = 'Euro'
-            allInstances = fa(f'[0-9\\,\\.]+ (?:{nounCurrency}|{ud(nounCurrency)})(?:e?s)?', inputSen)    # all should be uncaptured. Use (?:) instead of ()   <- captured
+            allInstances = fa(f'[0-9\\,\\.]+ (?:{nounCurrency}|{ud(nounCurrency)})(?:e?s)?', inputSen, flags=IGNORECASE)    # all should be uncaptured. Use (?:) instead of ()   <- captured
             for i in allInstances:
                 numberPart = fa(f'[0-9\\,\\.]+', i)
                 # Left or right ?
@@ -123,4 +123,23 @@ while True:
                     else:
                         rpmt = f'{symbol}{numberPart[0]}'
                     inputSen = s(i, rpmt, inputSen, flags=IGNORECASE)
+
+            # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+            if v['fractional']['name'] is not None:
+                nounFraction = v['fractional']['name']
+                allInstances = fa(f'[0-9\\,\\.]+ (?:{nounFraction}|{ud(nounFraction)})(?:e?s)?', inputSen, flags=IGNORECASE)    # all should be uncaptured. Use (?:) instead of ()   <- captured
+                for i in allInstances:
+                    numberPart = fa(f'[0-9\\,\\.]+', i)
+                    # Left or right ?
+                    if len(numberPart) > 0:
+                        if symbol.isalpha():  # Cyrillic and zloty are alphas !
+                            rpmt = f'{float(numberPart[0]) / v["fractional"]["numToBasic"]} {symbol}'
+                        else:
+                            rpmt = f'{symbol}{float(numberPart[0]) / v["fractional"]["numToBasic"]}'
+                        inputSen = s(i, rpmt, inputSen, flags=IGNORECASE)
     print(inputSen)
+    inputSen = input('Insert a sentence (e.g. I have ฿123,456.789): ')
+    for k, v in doneCurrencyNames.items():
+        # The hardest step is to make sure whether this is a currency. Some currency symbols are simply letters like L or KM.
+        pass
