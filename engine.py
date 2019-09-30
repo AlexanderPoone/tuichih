c08d3a1aad6e48f7c46b955c91170db0cc146274
Alexis Poon <alexander@link.cuhk.edu.hk>
2019-09-30 12:56:09 +0800

    .

--- engine.py
+++ engine.py
@@ -1,10 +1,21 @@
 import main, load, compile
-from flask import Flask
+from urllib.parse import unquote as u
+from flask import Flask, jsonify,request
+from rules.currency_interface import currency_interface
 app = Flask(__name__)
 
-@app.route("/")
+@app.route("/hello")
 def hello():
     return "Hello World!"
+
+@app.route("/")
+def currency():
+	# type = request.args.get('type')
+	input = request.args.get('input')
+	if input is None:
+		return jsonify([])
+	else:
+		return currency_interface().toSymbol(u(input))
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0')
--- rules/currency_scrape.py
+++ rules/currency_scrape.py
@@ -9,6 +9,7 @@ from pprint import pprint as pp
 from unidecode import unidecode as ud
 import json as j
 from hanziconv import HanziConv as hc
+from currency_interface import currency_interface
 
 pluralCache = {}
 
@@ -121,52 +122,4 @@ l = j.dumps(doneCurrencyNames)
 with open('currency_info.json', 'w') as x:
     x.write(l)
 pp(doneCurrencyNames)
-while True:
-    # inputSymb = input('Insert a currency symbol: ')
-    # for k, v in doneCurrencyNames.items():
-    #     if v['symbol'] == inputSymb:
-    #         print(k)
-    inputSen = input('Insert a sentence (e.g. I have 5,000 groszy.): ')
-    for k, v in doneCurrencyNames.items():
-        if v['symbol'] is not None:
-            if isinstance(v['symbol'],list):
-                symbol = v['symbol'][0]
-            else:
-                symbol = v['symbol']
-            nounCurrency = s('^.* ','',k,flags=IGNORECASE)                                                                      # fm('[a-z]+', k, flags=IGNORECASE) != None
-            if ' ' not in k:                                                                                                # exception: one-word currency: euro, bitcoin
-                nounCurrency = k
-            if v['plural'] is not None:
-                pluralPrefix = f'{v["plural"]}|{ud(v["plural"])}|'                                  # orders matter. It's always {plural|singular} rather than {singular|plural}
-            else:
-                pluralPrefix = ''
-            allInstances = fa(f'[0-9\\,\\.]+ (?:{pluralPrefix}{nounCurrency}|{ud(nounCurrency)})(?:e?s)?', inputSen, flags=IGNORECASE)    # all should be uncaptured. Use (?:) instead of ()   <- captured
-            for i in allInstances:
-                numberPart = fa(f'[0-9\\,\\.]+', i)
-                # Left or right ?
-                if len(numberPart) > 0:
-                    if symbol.isalpha():  # Cyrillic and zloty are alphas !
-                        rpmt = f'{numberPart[0]} {symbol}'
-                    else:
-                        rpmt = f'{symbol}{numberPart[0]}'
-                    inputSen = s(i, rpmt, inputSen, flags=IGNORECASE)
-
-            # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-
-            if v['fractional']['name'] is not None:
-                nounFraction = v['fractional']['name']
-                allInstances = fa(f'[0-9\\,\\.]+ (?:{nounFraction}|{ud(nounFraction)})(?:e?s)?', inputSen, flags=IGNORECASE)    # all should be uncaptured. Use (?:) instead of ()   <- captured
-                for i in allInstances:
-                    numberPart = fa(f'[0-9\\,\\.]+', i)
-                    # Left or right ?
-                    if len(numberPart) > 0:
-                        if symbol.isalpha():  # Cyrillic and zloty are alphas !
-                            rpmt = f'{float(numberPart[0].remove(",")) / v["fractional"]["numToBasic"]} {symbol}'
-                        else:
-                            rpmt = f'{symbol}{float(numberPart[0].remove(",")) / v["fractional"]["numToBasic"]}'
-                        inputSen = s(i, rpmt, inputSen, flags=IGNORECASE)
-    print(inputSen)
-    inputSen = input('Insert a sentence (e.g. I have ฿123,456.789): ')
-    for k, v in doneCurrencyNames.items():
-        # The hardest step is to make sure whether this is a currency. Some currency symbols are simply letters like L or KM.
-        pass
+currency_interface(load=False).interactive(json=doneCurrencyNames)
